2eae0e56707e16edf1a8b90f9a8ca2ce9712bb46
Gabriel Henrique Ferreira de Moraes <90928251+whybiel@users.noreply.github.com>
2022-02-03 20:45:03 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Desafio() {
+  return (
+    <div>
+      <h1>E é uma página de carros e vai ser super legal</h1>
+    </div>
+  );
+}
+
+export default Desafio;
